09918ab15fdad6fbc92383ceddc4b51ac4a7d7b5
Oluwaseun Isaac <72kulbranking@gmail.com>
2022-09-05 12:18:54 +0100

    first commit

---
+++ lecture.go
@@ -0,0 +1,41 @@
+package main
+
+//
+//func main() {
+//	a := 5
+//	if a < 3 {
+//		x := a
+//		fmt.Println("Number is less than 3", a, x)
+//	} else {
+//		fmt.Println("Number is greater than 3 ")
+//	}
+//
+//}
+
+//func mai n() {
+//	names := []string{"Seun", "Tobe", "Tope"}
+//	for i := 0; i < len(names); i++ {
+//		if names[i] == "Seun" {
+//			fmt.Println("Gotcha")
+//			continue
+//		}
+//		fmt.Println(names[i])
+//	}
+//
+//}
+
+//func main() {
+//	i := 0
+//	for i < 6 {
+//		fmt.Println(i)
+//		i++
+//
+//	}
+//
+//}
+
+//func main() {
+//	cars := []string{"Benz", "Toyota", "Lambo"}
+//	cars[1] = "Asto"
+//	fmt.Println(cars)
+//
